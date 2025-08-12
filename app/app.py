65d085a93b160b9bbe7001af8e4a59bc55fe0789
runalp97 <as4922011@gmail.com>
2025-08-12 13:38:57 +0530

    files updated new changes

--- app/app.py
+++ app/app.py
@@ -1,9 +1,16 @@
 from flask import Flask
+from prometheus_flask_exporter import PrometheusMetrics
+
 app = Flask(__name__)
+metrics = PrometheusMetrics(app)
 
 @app.route('/')
 def home():
     return "Hello from Flask App - CI/CD Automated!"
+
+@app.route('/health')
+def health():
+    return "OK"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
